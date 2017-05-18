bcfd80c414175f87507db2c66ec9d6c2b7f8d66e
ravidevkhile <ravidevkhile@gmail.com>
2017-05-18 08:41:26 +0530

    Ag- Grid Grouping Example With Other Sample code

---
+++ Ag - Grid/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+import {LicenseManager} from "ag-grid-enterprise/main";
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent {
+    
+}
---
+++ Ag - Grid/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+// ag-grid
+import {AgGridModule} from "ag-grid-angular/main";
+// application
+import {AppComponent} from "./app.component";
+// rich grid
+import {RichGridComponent} from "./rich-grid-example/rich-grid.component";
+import {DateComponent} from "./date-component/date.component";
+import {HeaderComponent} from "./header-component/header.component";
+import {HeaderGroupComponent} from "./header-group-component/header-group.component";
+import {GroupRowComponent} from "./grouped-row-example/group-row-renderer.component";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AgGridModule.withComponents(
+            [
+                DateComponent,
+                HeaderComponent,
+                HeaderGroupComponent
+            ]
+        )
+    ],
+    declarations: [
+        AppComponent,
+        RichGridComponent,
+        DateComponent,
+        HeaderComponent,
+        HeaderGroupComponent,GroupRowComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
